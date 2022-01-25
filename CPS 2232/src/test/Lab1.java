d56a5ed90f2737a451a080b05093ec39b4fdf89a
abiodunobafemi <abiodunobafemi@abioduns-air.fios-router.home>
2022-01-25 17:36:42 -0500

    "Commit 2

---
+++ CPS 2232/src/test/Lab1.java
@@ -0,0 +1,18 @@
+/******************************************************************************* *
+* Kean University
+* Spring 2022
+* Course: CPS*2232 – Data Structures
+* Author: Abiodun Obafemi, CPS 2232, 01
+* Homework (or Lab): Lab 1
+* Program Description: Prints "This is Lab 1"
+*************************************************************************/
+package test;
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+		System.out.print("This is Lab 1");
+
+	}
+
+}
